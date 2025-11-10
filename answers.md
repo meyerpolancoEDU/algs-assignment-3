fd2ca4dc2257fc6d592a7cf878a4405cb28a948b
meyerpolancoEDU <mpolanco1@tulane.edu>
2025-11-10 17:33:20 -0600

    Add files via upload

--- answers.md
+++ answers.md
@@ -1,8 +1,25 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:**Meyer Polanco
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+1a) A greedy algorithm for this problem is to continuously choose the largest coin that you can less than the remaining distance to N. For example, if N = 13, first choose 8 (8 < 13 < 16), then choose 4 (4 < 5 < 8), then end with 1 (1 = 1).
+
+1b) Greedy choice: Let 2^k be the largest coin <= the remaining amount N. In some optimal solution for N, there is at least one 2^k coin. Otherwise, that solution would make value at least 2^k using only coins of value <= 2^(k-1), which requires at least two coins; replacing those by one 2^k coin decreases the coin count, contradicting optimality unless a 2^k coin is used. So, choosing 2^k greedily is optimal
+
+Optimal substructure: After taking one 2^k coin, the remainder is N' = N - 2^k. If the coins used for N' were not an optimal solution to N', we could replace them with an optimal solution and reduce the total number of coins, contradicting optimality. Therefore an optimal solution for R consists of a 2^k coin plus an optimal solution for N'.
+
+Together, these imply the greedy algorithm is the optimal solution.
+
+1c) The work and span would both be O(logn). We will need logn coins total in the worst case, because the coins have value 2^k. And, because choosing a coin is dependant on the previous coins you took (how much of N is remaining), span is also logn.
+
+2a) Lets say Fortuito has coins for values 1, 8, 10 and we are trying to get value 16. Our greedy algorithm will direct us to choose a 10-coin, then 6 1-coins for a total of 7 coins. But the actual optimal solution is 2 8-coins.
+
+2b) Let OPT(N) be the minimum number of coins needed to make N using the given denominations. If an optimal solution for N uses some first coin d (where d <= N), then the remaining amount N - d must itself be solved optimally; otherwise, replacing the remainder by a better solution would reduce the total coin count, contradicting optimality. Therefore OPT(N) = min over all d in D with d <= N of (1 + OPT(N - d)), with base case OPT(0) = 0. This proves the problem has optimal substructure.
+
+2c) Bottom-up algorithm: Create an array OPT[0...N] with OPT[0] = 0. For a from 1 to N: set OPT[a] to the minimum, over coins d in D with d <= a, of 1 + OPT[a - d] (if no coin applies for a, then a is not makeable). Optionally keep PREV[a] to reconstruct the coins. If no coin applies for N, change is impossible; otherwise OPT[N] is the minimum number of coins.
+
+Work and span: Work is O(n). Span is O(n).
--- main.py
+++ main.py
@@ -38,10 +38,108 @@ def med_top_down(S, T, MED={}):
     return MED[(S, T)]
     
 def fast_MED(S, T):
-    # TODO -  implement top-down memoization
-    pass
+    '''Bottom-up memoization implementation of MED'''
+    m = len(S)
+    n = len(T)
+    
+    # Create a table to store edit distances
+    table = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    # Base cases: filling first row and column
+    # table[0][j] = j
+    for j in range(n + 1):
+        table[0][j] = j
+    
+    # table[i][0] = i
+    for i in range(m + 1):
+        table[i][0] = i
+    
+    # Fill the table bottom-up
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if S[i-1] == T[j-1]:
+                # Characters match, no cost
+                table[i][j] = table[i-1][j-1]
+            else:
+                # Characters don't match, choose minimum of insert or delete
+                insert = table[i][j-1] + 1  # Insert character from T
+                delete = table[i-1][j] + 1  # Delete character from S
+                table[i][j] = min(insert, delete)
+
+    return table[m][n]
+
+def align_helper(S, T):
+    """Helper function, same as fast_MED but returns the table instead of the edit distance"""
+    m = len(S)
+    n = len(T)
+    
+    table = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    for j in range(n + 1):
+        table[0][j] = j
+    
+    for i in range(m + 1):
+        table[i][0] = i
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if S[i-1] == T[j-1]:
+                table[i][j] = table[i-1][j-1]
+            else:
+                insert = table[i][j-1] + 1  # Insert character from T
+                delete = table[i-1][j] + 1  # Delete character from S
+                table[i][j] = min(insert, delete)
+
+    return table
+    
 
 def fast_align_MED(S, T):
-    # TODO - keep track of alignment
-    pass
+    '''Returns aligned versions of S and T showing the edit operations'''
+    # Get the DP table using the helper function
+    table = align_helper(S, T)
+    
+    m = len(S)
+    n = len(T)
+    
+    # Traceback from table[m][n] to reconstruct the alignment
+    align_S = []
+    align_T = []
+    i, j = m, n
+    
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and S[i-1] == T[j-1]:
+            # Characters match, align them
+            align_S.append(S[i-1])
+            align_T.append(T[j-1])
+            i -= 1
+            j -= 1
+        elif i > 0 and j > 0:
+            # Characters don't match, check which operation to use
+            if table[i][j] == table[i-1][j] + 1:
+                # Delete from S (gap in T)
+                align_S.append(S[i-1])
+                align_T.append('-')
+                i -= 1
+            else:
+                # Insert from T (gap in S)
+                align_S.append('-')
+                align_T.append(T[j-1])
+                j -= 1
+        elif j > 0:
+            # Only T has remaining characters - insert them
+            align_S.append('-')
+            align_T.append(T[j-1])
+            j -= 1
+        else:  # i > 0
+            # Only S has remaining characters - delete them
+            align_S.append(S[i-1])
+            align_T.append('-')
+            i -= 1
+    
+    # Reverse since we built the alignment backwards
+    align_S.reverse()
+    align_T.reverse()
+    
+    return (''.join(align_S), ''.join(align_T))
 
+print(fast_align_MED("kookaburra", "kookybird"))
